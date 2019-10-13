25af205b06fe012ad6bb7f3f83e569b7472553a5
Sam Barker <samuel.e.barker@googlemail.com>
2019-10-13 17:47:21 +0100

    added start menu

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { GameApp } from './app/App'
 import { gameWidth, gameHeight } from './config'
 
-const myGame = new GameApp(document.body, gameWidth, gameHeight)
+const menu = document.querySelector('#menu')
+const inGame = document.querySelector('#in-game')
+menu.querySelector('#play').addEventListener('click', () => {
+  menu.style.display = 'none'
+  inGame.style.display = 'block'
+  const myGame = new GameApp(document.body, gameWidth, gameHeight)
+})
